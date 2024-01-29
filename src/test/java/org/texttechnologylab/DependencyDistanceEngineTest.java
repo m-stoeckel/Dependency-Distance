3d12247988fc9d99841f10a67b56775f75de0bbe
Manuel Stoeckel <manuel.stoeckel@em.uni-frankfurt.de>
2024-01-29 14:38:44 +0100

    wip

--- src/test/java/org/texttechnologylab/DependencyDistanceEngineTest.java
+++ src/test/java/org/texttechnologylab/DependencyDistanceEngineTest.java
@@ -1,9 +1,11 @@
 package org.texttechnologylab;
 
+import de.tudarmstadt.ukp.dkpro.core.api.resources.CompressionMethod;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.DUUIComposer;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.*;
+import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIPipelineComponent;
+import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIUIMADriver;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.io.DUUIAsynchronousProcessor;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.io.reader.DUUIFileReader;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.lua.DUUILuaContext;
@@ -16,13 +18,15 @@ import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDesc
 public class DependencyDistanceEngineTest {
 
     @Test
-    public void testFolder() {
+    public void testProcess() {
         try {
-            int pScale = Integer.parseInt(System.getProperty("scale", "1"));
             String pInput = System.getProperty("input", "src/test/resources");
             String pEnding = System.getProperty("ending", ".xmi.gz");
-            boolean pFailOnError = Boolean.parseBoolean(System.getProperty("failOnError", "false"));
+            int pScale = Integer.parseInt(System.getProperty("scale", "1"));
+
             String pOutput = System.getProperty("output", "target/output/");
+
+            boolean pFailOnError = Boolean.parseBoolean(System.getProperty("failOnError", "true"));
 
             DUUIAsynchronousProcessor processor = new DUUIAsynchronousProcessor(new DUUIFileReader(pInput, pEnding));
 
@@ -31,11 +35,8 @@ public class DependencyDistanceEngineTest {
                     .withWorkers(pScale)
                     .withLuaContext(new DUUILuaContext().withJsonLibrary());
 
-            DUUIDockerDriver dockerDriver = new DUUIDockerDriver();
             DUUIUIMADriver uimaDriver = new DUUIUIMADriver();
-            DUUIRemoteDriver remoteDriver = new DUUIRemoteDriver();
-            DUUISwarmDriver swarmDriver = new DUUISwarmDriver();
-            composer.addDriver(dockerDriver, remoteDriver, uimaDriver, swarmDriver);
+            composer.addDriver(uimaDriver);
 
             Path path = Path.of(pOutput);
             path.toFile().mkdir();
@@ -43,7 +44,53 @@ public class DependencyDistanceEngineTest {
             DUUIPipelineComponent dependency = new DUUIUIMADriver.Component(
                     createEngineDescription(
                             DependencyDistanceEngine.class,
-                            DependencyDistanceEngine.PARAM_OUTPUT, pOutput,
+                            DependencyDistanceEngine.PARAM_TARGET_LOCATION, pOutput,
+                            DependencyDistanceEngine.PARAM_OVERWRITE, true,
+                            DependencyDistanceEngine.PARAM_FAIL_ON_ERROR, pFailOnError
+                    )
+            ).withScale(pScale).build();
+
+            composer.add(dependency);
+            composer.run(processor, "mDD");
+            composer.shutdown();
+
+            Assertions.assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void testCompression() {
+        try {
+            String pInput = System.getProperty("input", "src/test/resources");
+            String pEnding = System.getProperty("ending", ".xmi.gz");
+            int pScale = Integer.parseInt(System.getProperty("scale", "1"));
+
+            String pOutput = System.getProperty("output", "target/output/");
+
+            boolean pFailOnError = Boolean.parseBoolean(System.getProperty("failOnError", "true"));
+
+            DUUIAsynchronousProcessor processor = new DUUIAsynchronousProcessor(new DUUIFileReader(pInput, pEnding));
+
+            DUUIComposer composer = new DUUIComposer()
+                    .withSkipVerification(true)
+                    .withWorkers(pScale)
+                    .withLuaContext(new DUUILuaContext().withJsonLibrary());
+
+            DUUIUIMADriver uimaDriver = new DUUIUIMADriver();
+            composer.addDriver(uimaDriver);
+
+            Path path = Path.of(pOutput);
+            path.toFile().mkdir();
+
+            DUUIPipelineComponent dependency = new DUUIUIMADriver.Component(
+                    createEngineDescription(
+                            DependencyDistanceEngine.class,
+                            DependencyDistanceEngine.PARAM_TARGET_LOCATION, pOutput,
+                            DependencyDistanceEngine.PARAM_OVERWRITE, true,
+                            DependencyDistanceEngine.PARAM_COMPRESSION, CompressionMethod.BZIP2,
                             DependencyDistanceEngine.PARAM_FAIL_ON_ERROR, pFailOnError
                     )
             ).withScale(pScale).build();
--- src/test/java/org/texttechnologylab/Run.java
+++ src/test/java/org/texttechnologylab/Run.java
@@ -1,15 +1,19 @@
 package org.texttechnologylab;
 
+import de.tudarmstadt.ukp.dkpro.core.api.resources.CompressionMethod;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.DUUIComposer;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.connection.mongodb.MongoDBConfig;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.*;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.io.DUUIAsynchronousProcessor;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.lua.DUUILuaContext;
 import org.texttechnologylab.mdd.DependencyDistanceEngine;
 import org.texttechnologylab.parliament.duui.DUUIGerParCorReader;
 
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 
 public class Run {
@@ -17,36 +21,31 @@ public class Run {
     @Test
     public void GerParCor() {
         try {
-            int pScale = Integer.parseInt(System.getProperty("scale", "8"));
             String pMongoDbConfigPath = System.getProperty("config", "src/main/resources/mongodb.ini");
             String pFilter = System.getProperty("filter", "{}");
-            boolean pFailOnError = Boolean.parseBoolean(System.getProperty("failOnError", "false"));
+
             String pOutput = System.getProperty("output", "/storage/projects/stoeckel/syntactic-language-change/mdd/");
+            boolean pOverwrite = Boolean.parseBoolean(System.getProperty("overwrite", "false"));
+            CompressionMethod pCompression = CompressionMethod.valueOf(System.getProperty("compression", "NONE"));
+
+            boolean pFailOnError = Boolean.parseBoolean(System.getProperty("failOnError", "false"));
 
             MongoDBConfig mongoDbConfig = new MongoDBConfig(pMongoDbConfigPath);
-            DUUIAsynchronousProcessor processor = new DUUIAsynchronousProcessor(new DUUIGerParCorReader(mongoDbConfig, pFilter));
+            DUUIGerParCorReader duuiGerParCorReader = new DUUIGerParCorReader(mongoDbConfig, pFilter);
 
-            DUUIComposer composer = new DUUIComposer()
-                    .withSkipVerification(true)
-                    .withWorkers(pScale)
-                    .withLuaContext(new DUUILuaContext().withJsonLibrary());
+            AnalysisEngine engine = createEngine(
+                    DependencyDistanceEngine.class,
+                    DependencyDistanceEngine.PARAM_TARGET_LOCATION, pOutput,
+                    DependencyDistanceEngine.PARAM_OVERWRITE, pOverwrite,
+                    DependencyDistanceEngine.PARAM_COMPRESSION, pCompression,
+                    DependencyDistanceEngine.PARAM_FAIL_ON_ERROR, pFailOnError
+            );
 
-            DUUIDockerDriver dockerDriver = new DUUIDockerDriver();
-            DUUIUIMADriver uimaDriver = new DUUIUIMADriver();
-            DUUIRemoteDriver remoteDriver = new DUUIRemoteDriver();
-            DUUISwarmDriver swarmDriver = new DUUISwarmDriver();
-            composer.addDriver(dockerDriver, remoteDriver, uimaDriver, swarmDriver);
-            DUUIPipelineComponent dependency = new DUUIUIMADriver.Component(
-                    createEngineDescription(
-                            DependencyDistanceEngine.class,
-                            DependencyDistanceEngine.PARAM_OUTPUT, pOutput,
-                            DependencyDistanceEngine.PARAM_FAIL_ON_ERROR, pFailOnError
-                    )
-            ).withScale(pScale).build();
-
-            composer.add(dependency);
-            composer.run(processor, "mDD");
-            composer.shutdown();
+            JCas jCas = JCasFactory.createJCas();
+            while (duuiGerParCorReader.hasNext()) {
+                duuiGerParCorReader.getNextCas(jCas);
+                engine.process(jCas);
+            }
 
             Assertions.assertTrue(true);
         } catch (Exception e) {
